8f7abb3db327a50254d7dad30bb9666978f8563c
Jarron Cho <jarroncho@ptcom.com.tw>
2024-09-13 12:29:53 +0800

    add w1 example
    hello world

---
+++ teacher/w1/hello_world.py
@@ -0,0 +1,20 @@
+
+import sys;
+
+# hello world 
+
+print('hello world')
+
+
+# print hellowrld with variable 
+def hello():
+    print('hello world in function')
+
+
+def showpythonversion():
+    print('python version is :', sys.version)
+
+# call hello function
+hello()
+
+showpythonversion()
