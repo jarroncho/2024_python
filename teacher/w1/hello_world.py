ed21de8c882f2a75971d961d0dd85a52ebd7f962
yzu eeb <yzueeb@outlook.com>
2024-09-20 15:40:59 +0800

    1100561_w1

--- teacher/w1/hello_world.py
+++ teacher/w1/hello_world.py
@@ -1,14 +1,6 @@
-
 import sys;
-# hello world
-print('hello world for Jarron')
-# print hellowrld with variable
-def hello(name, id):
-    print('hello world in function')
-    print('name is :', name)
-    print('id is :', id)
-def showpythonversion():
-    print('python version is :', sys.version)
-# call hello function
-hello(name = '夏子翔', id = 1100561)
-showpythonversion()
+def my_function(name,id):
+    print(name,id)
+    print('name is ',name)
+    print('id is ',id)
+my_function(name='夏子翔',id=1100561)
