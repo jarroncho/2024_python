95cd6fdd99e24a1922fe1e7428b00c4c0a8d0237
benson3333 <s1111205@mail.yzu.edu.tw>
2024-09-20 15:41:33 +0800

    Add files via upload

---
+++ students/w1/1111205_hello_world.py
@@ -0,0 +1,23 @@
+
+import sys;
+
+# hello world 
+
+print('hello world for 1111205')
+
+
+
+# print hellowrld with variable 
+def hello(姓名,ID):
+    print('hello world in function')
+    print('Name:',姓名)
+    print('ID:',ID)
+
+
+def showpythonversion():
+    print('python version is :', sys.version)
+
+# call hello function
+hello(姓名 = '何秉儒',ID = 1111205)
+
+showpythonversion()
