bf31b83c981ae878db4cd9888ca29ca0dbe154f3
yzu eeb <yzueeb@outlook.com>
2024-09-20 15:46:53 +0800

    try commit

--- teacher/w1/hello_world.py
+++ teacher/w1/hello_world.py
@@ -1,6 +1,11 @@
-import sys;
-def my_function(name,id):
-    print(name,id)
-    print('name is ',name)
-    print('id is ',id)
-my_function(name='夏子翔',id=1100561)
+
+# print hellowrld with variable 
+def hello():
+    print('Hello World from Root hello function7')
+    print('Hello World ')
+
+# call hello function
+hello()
+
+
+
