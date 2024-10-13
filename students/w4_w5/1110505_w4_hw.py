b26185eb0decdf92f5d205a3f3606369ae5124a1
Tonhonew <a0905354747@gmail.com>
2024-10-13 20:24:30 +0800

    Modify assignment

--- students/w4_w5/1110505_w4_hw.py
+++ students/w4_w5/1110505_w4_hw.py
@@ -29,27 +29,12 @@ class YahooStock():
         price_div = self.stock_soup.find('div', {'class': 'container yf-1tejb6'})
         price_span = price_div.find('span').text
         return price_span
-    
 
-    #取得股價下跌幅度
-    def GetNegativeChange(self):
-        change_div=self.stock_soup.find('div', {'class': 'container yf-1tejb6'})
-        change_span=change_div.find('span', {'class':'txt-negative change yf-1tejb6'}).text
-        return change_span
-    
 
-    #取得股價不變的數字
     def GetChange(self):
-        change_div=self.stock_soup.find('div', {'class': 'container yf-1tejb6'})
-        change_span=change_div.find('span', {'class':'change yf-1tejb6'}).text
+        change_fin=self.stock_soup.find('fin-streamer', {'class': 'priceChange yf-1tejb6'})
+        change_span=change_fin.find('span').text
         return change_span
-
-
-    #取得股價上漲幅度
-    def GetPositiveChange(self):
-        change_div=self.stock_soup.find('div', {'class': 'container yf-1tejb6'})
-        change_span=change_div.find('span', {'class':'txt-positive change yf-1tejb6'}).text
-        return change_span 
     
 
     def GetDescription(self):
@@ -66,16 +51,16 @@ if __name__ == "__main__":
     #注意，GetNegativeChange()、GetChange()、GetPositiveChange()要依照當天股價的上漲和下跌幅度而調整使用
     print('Symbol\tStock Price\tChange\tDescription')
     yahoo_stock_src.FetchStockInfo('AAPL')
-    print('AAPL'+'\t'+yahoo_stock_src.GetStockPrice()+'\t\t'+yahoo_stock_src.GetNegativeChange()+'\t'+yahoo_stock_src.GetDescription())
+    print('AAPL'+'\t'+yahoo_stock_src.GetStockPrice()+'\t\t'+yahoo_stock_src.GetChange()+'\t'+yahoo_stock_src.GetDescription())
     yahoo_stock_src.FetchStockInfo('GOOGL')
     print('GOOGL'+'\t'+yahoo_stock_src.GetStockPrice()+'\t\t'+yahoo_stock_src.GetChange()+'\t'+yahoo_stock_src.GetDescription())
     yahoo_stock_src.FetchStockInfo('AMZN')
-    print('AMZN'+'\t'+yahoo_stock_src.GetStockPrice()+'\t\t'+yahoo_stock_src.GetNegativeChange()+'\t'+yahoo_stock_src.GetDescription())
+    print('AMZN'+'\t'+yahoo_stock_src.GetStockPrice()+'\t\t'+yahoo_stock_src.GetChange()+'\t'+yahoo_stock_src.GetDescription())
     yahoo_stock_src.FetchStockInfo('INTC')
-    print('INTC'+'\t'+yahoo_stock_src.GetStockPrice()+'\t\t'+yahoo_stock_src.GetNegativeChange()+'\t'+yahoo_stock_src.GetDescription())
+    print('INTC'+'\t'+yahoo_stock_src.GetStockPrice()+'\t\t'+yahoo_stock_src.GetChange()+'\t'+yahoo_stock_src.GetDescription())
     yahoo_stock_src.FetchStockInfo('AMD')
-    print('AMD'+'\t'+yahoo_stock_src.GetStockPrice()+'\t\t'+yahoo_stock_src.GetPositiveChange()+'\t'+yahoo_stock_src.GetDescription())
+    print('AMD'+'\t'+yahoo_stock_src.GetStockPrice()+'\t\t'+yahoo_stock_src.GetChange()+'\t'+yahoo_stock_src.GetDescription())
     yahoo_stock_src.FetchStockInfo('NVDA')
-    print('NVDA'+'\t'+yahoo_stock_src.GetStockPrice()+'\t\t'+yahoo_stock_src.GetPositiveChange()+'\t'+yahoo_stock_src.GetDescription())
+    print('NVDA'+'\t'+yahoo_stock_src.GetStockPrice()+'\t\t'+yahoo_stock_src.GetChange()+'\t'+yahoo_stock_src.GetDescription())
     yahoo_stock_src.FetchStockInfo('TSLA')
-    print('TSLA'+'\t'+yahoo_stock_src.GetStockPrice()+'\t\t'+yahoo_stock_src.GetNegativeChange()+'\t'+yahoo_stock_src.GetDescription())
+    print('TSLA'+'\t'+yahoo_stock_src.GetStockPrice()+'\t\t'+yahoo_stock_src.GetChange()+'\t'+yahoo_stock_src.GetDescription())
