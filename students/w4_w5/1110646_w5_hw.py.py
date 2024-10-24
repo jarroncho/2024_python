e342860a7a5bf87d008acf2607caba2dff247ca8
zzh710 <s1110646@mail.yzu.edu.tw>
2024-10-24 22:32:31 +0800

    Add files via upload

---
+++ students/w4_w5/1110646_w5_hw.py.py
@@ -0,0 +1,77 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from bs4 import BeautifulSoup
+import pandas as pd
+import time
+
+
+class YahooStock():
+    def __init__(self):
+        self.browser = webdriver.Chrome()
+        start_time = time.time()
+        self.browser.get("https://finance.yahoo.com/quote/")
+
+    def __del__(self):
+        self.browser.quit()
+
+    def FetchStockInfo(self, stock_name):
+        locator = (By.ID, "ybar-sbq")
+        self.input = WebDriverWait(self.browser, 30).until(
+            EC.presence_of_element_located(locator),
+            "等待搜尋輸入框超時"
+        )
+
+        locator = (By.ID, "ybar-search")
+        self.search = WebDriverWait(self.browser, 30).until(
+            EC.presence_of_element_located(locator),
+            "等待搜尋按鈕超時"
+        )
+
+        self.input.clear()  
+        self.input.send_keys(stock_name)
+
+        WebDriverWait(self.browser, 30).until(
+            EC.element_to_be_clickable(self.search)
+        )
+
+        self.search.click()
+        start_time = time.time()
+        WebDriverWait(self.browser, 30).until(
+            EC.staleness_of(self.search)  
+        )
+
+        self.stock_soup = BeautifulSoup(self.browser.page_source, "lxml")
+
+    def GetStockPrice(self):
+        price_div = self.stock_soup.find('fin-streamer', {'class': 'livePrice yf-1tejb6'})
+        price_span = price_div.find('span').text
+        return price_span
+    
+    def GetStockChange(self):
+        change_div = self.stock_soup.find('fin-streamer', {'class': 'priceChange yf-1tejb6'})
+        change_span = change_div.find('span').text
+        return change_span
+
+    def GetStockDescription(self):
+        Description_div = self.stock_soup.find('div', {'class': 'left yf-1s1umie wrap'})
+        Description_span = Description_div.find('h1', {'class':'yf-xxbei9'}).text
+        return Description_span
+
+if __name__ == "__main__":
+    yahoo_stock_src = YahooStock()
+
+    stock_symbols = ['AAPL', 'GOOGL', 'AMZN', 'INTC','AMD','NVDA','TSLA']  
+
+    result = []
+
+    for stock_symbol in stock_symbols:
+        yahoo_stock_src.FetchStockInfo(stock_symbol)
+        price = yahoo_stock_src.GetStockPrice()
+        change = yahoo_stock_src.GetStockChange()
+        description = yahoo_stock_src.GetStockDescription()
+        result.append((stock_symbol, price, change, description))
+
+    df = pd.DataFrame(result, columns=["Symbol", "Stock Price", "Change", "Discription"])
+    df.to_excel("stock_search.xlsx", sheet_name="stock", index=False)
