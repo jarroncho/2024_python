c2dabac847888a4b47b901b89813328af152af1f
Euai19 <126462085+Euai19@users.noreply.github.com>
2024-10-30 18:52:26 +0800

    Update 1100666_w2.py

--- students/w2/1100666_w2.py
+++ students/w2/1100666_w2.py
@@ -1,6 +1,4 @@
-
 import sys;   
-
 
 """
 姓名	國文	英文	數學	理化	總分	平均	名次
@@ -20,38 +18,21 @@ grade_list = [['John', 72, 88, 88, 84, 0, 0, 0],
               ['Alice', 92, 79, 93, 89, 0, 0, 0], 
               ['Match', 81, 72, 62, 70, 0, 0, 0], 
               ['Sunny', 78, 77, 51, 72, 0, 0, 0]]
-for i in range(len(grade_list)):
-    grade_list[i][5]=grade_list[i][1]+grade_list[i][2]+grade_list[i][3]+grade_list[i][4]
-    grade_list[i][6]=(grade_list[i][5])/4
-
-grade_list_sort = sorted(grade_list, key=lambda x: x[5],reverse=True)
-print(grade_list_sort)
-
-for i in range(len(grade_list_sort)):    
-    grade_list_sort[i][7]=i+1
-
-a=[0,0,0,0,0,0,0]
-b=[0,0,0,0,0,0,0]
+arr=[]
 
 for i in range(len(grade_list)):
-    grade_list[i][5] = grade_list[i][1] + grade_list[i][2] + grade_list[i][3] + grade_list[i][4]
-    grade_list[i][6] = grade_list[i][5] / 4
-    a[i]=grade_list[i][6]
+  grade_list[i][5]=grade_list[i][4]+grade_list[i][3]+grade_list[i][2]+grade_list[i][1]
+  grade_list[i][6]=float(grade_list[i][5]/4)
+  arr.append(grade_list[i][6])
 
-c=sorted(a)
-
-for j in range(7): 
-    for i in range(7):
-        if a[j] == c[i]:
-            b[j]=7-i
-            break
-    
-        
-
-for i in range(len(grade_list)):
-    grade_list[i][7]=b[i]
-
-
+arr=sorted(arr,reverse=True)
+print(arr)
+for j in range(len(grade_list)):
+   for i,num in enumerate(arr):
+      if grade_list[j][6]==num:
+         grade_list[j][7]=i+1
+      
+                  
 
 print('姓名\t國文\t英文\t數學\t理化\t總分\t平均\t名次')
 print('------------------------------------------------------------')
