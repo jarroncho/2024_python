5e6e99f1a23d1057ff15bffffd7fb89f82a42276
田和叡 <s1110650@mail.yzu.edu.tw>
2024-11-18 20:26:18 +0800

    Add files via upload

---
+++ students/w6_w7/1110650_W6/student/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('simple/', views.simple_view, name='simple'),
+    path('template/', views.template_view, name='template'),
+    path('students/', views.student_list_view, name='student_list'),
+]
