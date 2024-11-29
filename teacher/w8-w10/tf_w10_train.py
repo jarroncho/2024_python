ac2b3895e6a94879eee5d9ba2c5f5d3b0bebacff
Jarron Cho <jarroncho@ptcom.com.tw>
2024-11-29 11:17:22 +0800

    W10 init

---
+++ teacher/w8-w10/tf_w10_train.py
@@ -0,0 +1,195 @@
+import shutil
+import random 
+import os
+import time
+from PIL import Image
+# Import the ImageDataGenerator class
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.models import Sequential 
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, BatchNormalization, Dropout, Dense, Flatten
+from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint, ReduceLROnPlateau , CSVLogger
+import matplotlib.pyplot as plt
+from sklearn.metrics import confusion_matrix as cm
+from sklearn.metrics import ConfusionMatrixDisplay
+
+
+original_dataset_path = 'PetImages'
+clean_dataset_path = 'working/Dataset'
+test_path = 'working/test'
+train_path = 'working/train'
+validation_path = 'working/validation'
+
+
+
+
+# Set the image dimensions and Batch size
+WIDTH = 128
+HEIGHT = 128
+IMG_SIZE = (WIDTH , HEIGHT)
+BATCH = 32
+#BATCH = 32
+
+# Create an ImageDataGenerator object for the validation set
+validation_datagen = ImageDataGenerator(rescale=1. / 255)
+# Create an ImageDataGenerator object for the test set
+test_datagen = ImageDataGenerator(rescale=1. / 255)
+# Create an ImageDataGenerator object for the training set
+
+train_datagen = ImageDataGenerator(
+    rescale=1. / 255,
+    rotation_range=15,
+    shear_range=0.2,
+    zoom_range=0.2,
+    horizontal_flip=True)
+
+train_generator = train_datagen.flow_from_directory(train_path, 
+                                                    target_size = IMG_SIZE,
+                                                    classes=['Cat' , 'Dog'],
+                                                    class_mode='binary',
+                                                    batch_size=BATCH,
+                                                    #save_to_dir=aug_data_path,
+                                                    #save_prefix='aug_',
+                                                    #save_format="jpg",
+                                                    seed = 1
+                                                    
+                                                    )
+print("train_generator: ",train_generator.class_indices)
+print("class: ", train_generator.num_classes)
+print("sample: ",train_generator.samples)
+
+validation_generator = validation_datagen.flow_from_directory(validation_path, 
+                                                    target_size = IMG_SIZE,
+                                                    classes=['Cat' , 'Dog'],
+                                                    class_mode='binary',
+                                                    batch_size=BATCH,
+                                                    seed = 1 )
+print("validation_generator: ",validation_generator.class_indices)
+
+test_generator = test_datagen.flow_from_directory(test_path, 
+                                                    target_size = IMG_SIZE,
+                                                    classes=['Cat' , 'Dog'],
+                                                    class_mode='binary',
+                                                    batch_size=BATCH,
+                                                    seed = 1 )
+print("test_generator: ",test_generator.class_indices)
+
+
+# Create a Sequential model
+model = Sequential()
+
+model.add(Conv2D(32, (3, 3), activation='relu', input_shape=(WIDTH, HEIGHT, 3)))
+model.add(BatchNormalization())
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.25))
+
+model.add(Conv2D(64, (3, 3), activation='relu'))
+model.add(BatchNormalization())
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.25))
+
+model.add(Conv2D(128, (3, 3), activation='relu'))
+model.add(BatchNormalization())
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.25))
+
+model.add(Conv2D(256, (3, 3), activation='relu'))
+model.add(BatchNormalization())
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.25))
+
+model.add(Flatten())
+model.add(Dense(512, activation='relu'))
+model.add(BatchNormalization())
+model.add(Dropout(0.25))
+model.add(Dense(1, activation='sigmoid'))
+
+# Show Model Structure
+model.summary()
+model.compile(loss='binary_crossentropy',
+              optimizer='adam',
+              metrics=['accuracy'])
+
+# Create a directory to store the models
+
+
+models_path = 'working/Models'
+
+os.makedirs(models_path,exist_ok=True)
+
+learning_rate_reduction = ReduceLROnPlateau(monitor='val_accuracy', 
+                                            patience= 4, 
+                                            verbose= 1, 
+                                            factor= 0.75, 
+                                            min_lr= 0.00001)
+
+early_stopping = EarlyStopping(patience = 10)
+
+"""checkpoint_loss = ModelCheckpoint(filepath = models_path + '/model_best_loss.hdf5',
+    monitor = "val_loss",
+    save_best_only = True, save_freq= 'epoch' )"""
+
+checkpoint_acc = ModelCheckpoint(filepath = models_path + '/model_best_acc.keras',
+    monitor = "val_accuracy",
+    save_best_only = True, save_freq= 'epoch' )
+
+filename='log.csv'
+history_logger= CSVLogger(filename, separator=",", append=True)
+
+
+callbacks = [learning_rate_reduction, early_stopping , #checkpoint_loss , 
+             checkpoint_acc, history_logger]
+
+
+
+
+#train the model
+#EPOCHS = 60
+EPOCHS = 60
+beg = int(time.time())
+
+history = model.fit(
+        train_generator,
+        steps_per_epoch = train_generator.samples  // BATCH,
+        epochs = EPOCHS,
+        validation_data = validation_generator,
+        validation_steps = validation_generator.samples // BATCH,
+        )
+        #callbacks = callbacks)
+
+end = int(time.time())
+t = end - beg
+hrs = t // 3600
+mins = (t - 3600 * hrs) // 60
+secs = t % 60
+print("training took {} hrs -- {} mins -- {} secs".format(hrs,mins,secs))
+model.save_weights(models_path + '/first_try.weights.h5')
+model.save(models_path + '/first_try.h5')
+
+#evaluate the model
+scores = model.evaluate(test_generator)
+
+#plot
+plt.plot(history.history['accuracy'])
+plt.plot(history.history['val_accuracy'])
+plt.title('model accuracy')
+plt.ylabel('accuracy')
+plt.xlabel('epoch')
+plt.legend(['train', 'val'], loc='upper left')
+plt.show()
+
+plt.plot(history.history['loss'])
+plt.plot(history.history['val_loss'])
+plt.title('model loss')
+plt.ylabel('loss')
+plt.xlabel('epoch')
+plt.legend(['train', 'val'], loc='upper left')
+plt.show()
+
+plt.plot(history.history['lr'])
+plt.title('Learning rate')
+plt.ylabel('learnin rate')
+plt.xlabel('epoch')
+plt.legend(['learning rate'], loc='upper left')
+plt.show()
+
+
