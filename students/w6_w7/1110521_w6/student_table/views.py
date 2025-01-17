5c5776876bd9fdb90ebf4f102162f4fa88556b3f
Anderson-Sun <anderson031117@gmail.com>
2025-01-17 12:47:57 +0800

    Add files via upload

---
+++ students/w6_w7/1110521_w6/student_table/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Student
+
+def simple_view(request):
+    return HttpResponse("Hello, World!")
+
+def template_view(request):
+    return render(request, 'master.html')
+
+def student_list_view(request):
+    students = Student.objects.all()
+    return render(request, 'student_list.html', {'students': students})
