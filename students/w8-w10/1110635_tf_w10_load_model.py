b2ed7deddf1a9ebdd4e7018077a12d3ad9dcd148
1110635 <s1110635@mail.yzu.edu.tw>
2025-01-17 11:49:19 +0800

    Add files via upload

---
+++ students/w8-w10/1110635_tf_w10_load_model.py
@@ -0,0 +1,154 @@
+import shutil
+import random 
+import os
+import time
+from PIL import Image
+# Import the ImageDataGenerator class
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.models import Sequential 
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, BatchNormalization, Dropout, Dense, Flatten
+from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint, ReduceLROnPlateau , CSVLogger
+from tensorflow.keras.models import load_model
+import numpy as np  
+
+import matplotlib.pyplot as plt
+from sklearn.metrics import confusion_matrix 
+from sklearn.metrics import ConfusionMatrixDisplay
+import cv2
+
+# Set the image dimensions and Batch size
+WIDTH = 128
+HEIGHT = 128
+IMG_SIZE = (WIDTH , HEIGHT)
+BATCH = 32
+
+original_dataset_path = 'PetImages'
+clean_dataset_path = 'working/Dataset'
+test_path = 'working/test'
+train_path = 'working/train'
+validation_path = 'working/validation'
+
+
+test_datagen = ImageDataGenerator(rescale=1. / 255)
+
+test_generator = test_datagen.flow_from_directory(test_path, 
+                                                    target_size = IMG_SIZE,
+                                                    classes=['Cat' , 'Dog'],
+                                                    class_mode='binary',
+                                                    batch_size=BATCH,
+                                                    seed = 1 )
+print("test_generator: ",test_generator.class_indices)
+
+# Specify the path to your saved model
+models_path = 'working/Models'
+model_path = models_path + '/first_try.h5'
+
+# Load the model
+model = load_model(model_path)
+
+
+
+#evaluate the model
+scores = model.evaluate(test_generator)
+
+
+
+labels = ["Cat","Dog"]
+
+plt.figure(figsize=(8, 5))
+
+    
+for j in range(20):
+    i = random.randint(0,120)
+    dog_test_img = cv2.imread(test_path + '/Dog'+'/'+
+                              os.listdir(test_path + '/Dog')[i])
+    #print(os.listdir(test_path + '/Dog')[5])
+    #print(type(dog_test_img))
+    #print(dog_test_img.shape)
+    dog_test_img = cv2.cvtColor(dog_test_img,cv2.COLOR_BGR2RGB)
+    plt.subplot(4, 5, j+1)
+    plt.imshow(dog_test_img)
+    plt.axis('off')
+    
+    dog_test_img = cv2.resize(dog_test_img,(128,128))
+    dog_test_img = np.reshape(dog_test_img,(1,128,128,3))
+    #print(dog_test_img.shape)
+    
+    results = model.predict(dog_test_img,verbose = 0)
+    results = np.squeeze(results)
+
+    plt.title(labels[results.astype(int)])
+
+    #print(results.astype(int))
+    #print(type(results))
+
+
+plt.figure(figsize=(8, 5))
+
+
+for j in range(20):
+    i = random.randint(0,1000)
+    cat_test_img = cv2.imread(train_path + '/Cat'+'/'+
+                              os.listdir(train_path + '/Cat')[i])
+    #print(os.listdir(test_path + '/Dog')[5])
+    #print(type(dog_test_img))
+    #print(dog_test_img.shape)
+    cat_test_img = cv2.cvtColor(cat_test_img,cv2.COLOR_BGR2RGB)
+    plt.subplot(4, 5, j+1)
+    plt.imshow(cat_test_img)
+    plt.axis('off')
+    
+    cat_test_img = cv2.resize(cat_test_img,(128,128))
+    cat_test_img = np.reshape(cat_test_img,(1,128,128,3))
+    #print(dog_test_img.shape)
+    
+    results = model.predict(cat_test_img,verbose = 0)
+    results = np.squeeze(results)
+    label_idx = np.round(results,1).astype(int)
+    plt.title(labels[label_idx])
+
+    #print(results.astype(int))
+    #print(type(results))
+
+
+for step in range( test_generator.samples // 32):
+    (x, y) = next(test_generator)
+    print(y)
+    print(y.shape)
+    print(type(y))
+    y = y.astype(int)
+    print(y)
+
+    #Predict
+    y_prediction = model.predict(x)
+    print(y_prediction)
+    y_prediction = np.round(y_prediction,1).astype(int)
+    print(y_prediction.shape)
+    y_prediction = np.reshape(y_prediction,y.shape)
+    print(y_prediction)
+
+
+ys = []
+y_predictions = []
+
+for step in range( test_generator.samples // 32):
+    (x, y) = next(test_generator)
+    y = y.astype(int)
+    ys = ys + list(y)
+
+    #Predict
+    y_prediction = model.predict(x)
+    #print(y_prediction)
+    y_prediction = np.round(y_prediction,1).astype(int)
+    #print(y_prediction.shape)
+    y_prediction = np.reshape(y_prediction,y.shape)
+    y_predictions  = y_predictions + list(y_prediction)
+    #print(y_prediction)
+    
+    # Calculate the confusion matrix
+    cm = confusion_matrix(ys, y_predictions)
+
+disp = ConfusionMatrixDisplay(confusion_matrix=cm, display_labels=labels)
+disp.plot()
+    
+plt.show()
